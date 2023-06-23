6139b65a99ef8a73211b3c4b19cf9d03c6e7b4ba
Sajib Mahmud <mdsajibmahmud71@gmail.com>
2023-06-23 16:51:03 +0600

    created models

--- bookshop/models.py
+++ bookshop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='static/image/')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='static/image/')
+    slug = models.SlugField(max_length=255)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Books'
+        ordering = ('-created',)
+        
+    def get_absolute_url(self):
+        return reverse('books:book_details', args=[self.slug])
+
+    def __str__(self):
+        return self.title
